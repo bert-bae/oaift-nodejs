1c9edf2ca4a636ff75fbc7fde7edecb35a6e6bc5
bert-bae <elbert.bae@gmail.com>
2023-10-24 14:42:23 -0700

    Remove fs promises from init

--- src/processors/ProjectCmd.ts
+++ src/processors/ProjectCmd.ts
@@ -1,4 +1,4 @@
-import fs from "fs/promises";
+import fs from "fs";
 import ftTemplate from "../templates/oaift-temp.config.json";
 import genTemplate from "../templates/oaigen-temp.config.json";
 import { prettifyJson } from "../utils/log";
@@ -18,31 +18,27 @@ export class ProjectCmd {
   }
 
   private async setup() {
-    try {
-      await fs.readdir("./projects");
-    } catch {
-      await fs.mkdir("./projects");
+    if (!fs.existsSync("./projects")) {
+      fs.mkdirSync("./projects");
     }
 
     const projectPath = `./projects/${this.opts.name}`;
-    let projectFolder;
-    try {
-      projectFolder = await fs.readdir(projectPath);
-    } catch {
-      await fs.mkdir(projectPath);
+    if (!fs.existsSync(projectPath)) {
+      fs.mkdirSync(projectPath);
     }
 
-    if (projectFolder?.length > 0) {
+    let projectFolder = fs.readdirSync(projectPath);
+    if (projectFolder.length) {
       throw new Error(
         `Project folder is not empty. If you want to proceed, please choose a different project name or delete the existing directory: ${projectPath}`
       );
     }
 
-    await fs.writeFile(
+    fs.writeFileSync(
       `${projectPath}/oaigen.config.json`,
       prettifyJson(genTemplate)
     );
-    await fs.writeFile(
+    fs.writeFileSync(
       `${projectPath}/oaift.config.json`,
       prettifyJson(ftTemplate)
     );
